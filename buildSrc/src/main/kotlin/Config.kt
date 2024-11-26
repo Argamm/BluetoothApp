af769819672ab86bc2a7aee6ca53498ad02a26db
Eddy Hovhannisyan <eh@exante.eu>
2024-11-26 13:29:39 +0400

    version increased

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -6,7 +6,7 @@ object Config {
     const val MIN_SDK_VERSION = 26
     const val TARGET_SDK_VERSION = 34
     const val VERSION_CODE = 2
-    const val VERSION_NAME = "1.0.3"
+    const val VERSION_NAME = "1.0.4"
     val JVM_TARGET = JavaVersion.VERSION_17.toString()
     const val KOTLIN_COMPILER_EXTENSION_VERSION = "1.5.13"
     const val EXCLUDES = "/META-INF/{AL2.0,LGPL2.1}"
